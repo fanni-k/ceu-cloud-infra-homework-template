d784cfe3c868544635993ff481b07eb4fca1a674
Your Name <strabancer@gmail.com>
2021-03-08 00:23:21 +0100

    bronze view SQL code added

--- bronze_views.sql
+++ bronze_views.sql
@@ -1,2 +1,10 @@
 -- ADD THE ATHENA SQL SCRIPT HERE WHICH CREATES THE `bronze_views` TABLE
- 
+ CREATE EXTERNAL TABLE
+fannik_homework.bronze_views (
+    article STRING,
+  views INT,
+  rank INT,
+    date DATE,
+    retrieved_at TIMESTAMP) 
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://student-2001405/de4/views/';
