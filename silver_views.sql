e6fffd34dd438b6f20e1ffe2139d62fe88c2c48e
Your Name <strabancer@gmail.com>
2021-03-08 00:25:52 +0100

    silver view code added

--- silver_views.sql
+++ silver_views.sql
@@ -1,2 +1,7 @@
 -- ADD THE ATHENA SQL SCRIPT HERE WHICH CREATES THE `silver_views` TABLE
- 
+ CREATE TABLE fannik_homework.silver_views
+    WITH (
+          format = 'PARQUET',
+          parquet_compression = 'SNAPPY',
+          external_location = 's3://student-2001405/de4/views_silver'
+    ) AS SELECT article, views, rank, date FROM fannik_homework.bronze_views;
